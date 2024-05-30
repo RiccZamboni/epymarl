6282099f21cbf5cfc5e725643d1649b67e32e129
atishdixit16 <atishdixit16@gmail.com>
2024-05-30 11:20:13 +0100

    Modify forward function for RNNESAgent and ACCriticES

--- src/modules/agents/rnn_es_agent.py
+++ src/modules/agents/rnn_es_agent.py
@@ -4,24 +4,27 @@ import torch as th
 # create RNNESAgent class that inherits from RNNSAgent
 
 class RNNESAgent(RNNNSAgent):
-    # define a modified forard function that accepts agent_id as an argument
+    # define a modified forward function that accepts agent_id as an argument
     # this function should return the action output such that only agent_id agent rollouts using ...
     # ...batch of all agents (instead each agent_id corresponding to each agent in the batch, as done in NonSharedMAC)
-    # e.g. def forward(self, inputs, hidden_state, agent_id):
 
     def forward(self, inputs, hidden_state, agent_id=None):
+
+        if agent_id is None:
+            raise ValueError("agent_id must be provided for RNNESAgent")
+        
         hiddens = []
         qs = []
         if inputs.size(0) == self.n_agents:
             for i in range(self.n_agents):
-                q, h = self.agents[i](inputs[i].unsqueeze(0), hidden_state[:, i])
+                q, h = self.agents[agent_id](inputs[i].unsqueeze(0), hidden_state[:, i])
                 hiddens.append(h)
                 qs.append(q)
             return th.cat(qs), th.cat(hiddens).unsqueeze(0)
         else:
             for i in range(self.n_agents):
                 inputs = inputs.view(-1, self.n_agents, self.input_shape)
-                q, h = self.agents[i](inputs[:, i], hidden_state[:, i])
+                q, h = self.agents[agent_id](inputs[:, i], hidden_state[:, i])
                 hiddens.append(h.unsqueeze(1))
                 qs.append(q.unsqueeze(1))
             return th.cat(qs, dim=-1).view(-1, q.size(-1)), th.cat(hiddens, dim=1)
--- src/modules/critics/ac_es.py
+++ src/modules/critics/ac_es.py
@@ -12,13 +12,17 @@ class ACCriticES(ACCriticNS):
     # This new forward function will only return the critic values with the critic for given agent_id
     # e.g. def forward(self, batch, t=None, agent_id):
     def forward(self, batch, t=None, agent_id=None):
+
+        if agent_id is None:
+            raise ValueError("agent_id must be provided for ACCriticES")
+
         inputs, bs, max_t = self._build_inputs(batch, t=t)
         qs = []
         for i in range(self.n_agents):
             if self.args.use_critic_rnn:
-                q = self.critics[i](inputs[:, :, i], max_t, bs)
+                q = self.critics[agent_id](inputs[:, :, i], max_t, bs)
             else:
-                q = self.critics[i](inputs[:, :, i])
+                q = self.critics[agent_id](inputs[:, :, i])
             qs.append(q.view(bs, max_t, 1, -1))
         q = th.cat(qs, dim=2)
         return q
