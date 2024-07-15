3237ce19b870dec38643225cafedd3c91a77e5c7
atishdixit16 <atishdixit16@gmail.com>
2024-07-15 11:17:28 +0100

    remove rnn_es agent from modules

--- src/modules/agents/__init__.py
+++ src/modules/agents/__init__.py
@@ -2,9 +2,7 @@ REGISTRY = {}
 
 from .rnn_agent import RNNAgent
 from .rnn_ns_agent import RNNNSAgent
-from .rnn_es_agent import RNNESAgent
 from .rnn_feature_agent import RNNFeatureAgent
 REGISTRY["rnn"] = RNNAgent
 REGISTRY["rnn_ns"] = RNNNSAgent
-REGISTRY["rnn_es"] = RNNESAgent
 REGISTRY["rnn_feat"] = RNNFeatureAgent
